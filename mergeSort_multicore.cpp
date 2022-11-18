8d9c20339540878cebae34c8d878d040952048cc
cha0xfox <IvSkvortsov@mts.ru>
2022-11-18 12:48:59 +0300

    multicore

--- mergeSort_multicore.cpp
+++ mergeSort_multicore.cpp
@@ -59,6 +59,126 @@ void randDataInitialization(int* a, int n) {
 	}
 }
 
+void DataDistr(int* arr, int arr_size, int*& pArr, int& pArr_size) {
+    int RestElem; //Number of rows, that haven't been distributed yet
+
+    MPI_Bcast(&arr_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    RestElem = arr_size;
+    for (int i = 0; i < ProcRank; i++) {
+        RestElem -= RestElem / (ProcNum - i);
+    }
+    pArr_size = RestElem / (ProcNum - ProcRank);
+
+    pArr = new int[pArr_size];
+
+    int* send_cnt = new int[ProcNum];
+    int* send_disp = new int[ProcNum];
+    int cnt = arr_size / ProcNum;
+    RestElem = arr_size;
+
+    send_cnt[0] = cnt;
+    send_disp[0] = 0;
+
+    for (int i = 1; i < ProcNum; i++) {
+        RestElem -= cnt;
+        cnt = RestElem / (ProcNum - i);
+        send_cnt[i] = cnt;
+        send_disp[i] = send_disp[i - 1] + send_cnt[i - 1];
+    }
+
+    MPI_Scatterv(arr, send_cnt, send_disp, MPI_INT, pArr, send_cnt[ProcRank], MPI_INT, 0, MPI_COMM_WORLD);
+
+    delete[] send_cnt;
+    delete[] send_disp;
+
+}
+
+void ProccesInitialization(int*& arr, int& arr_size) {
+	if (ProcRank == 0) {
+		do {
+			cout << "\nEnter size of array : ";
+			cin >> arr_size;
+			if (arr_size < ProcNum) cout << "\n Size of array must be greater than ProcNum\n";
+		} while (arr_size < ProcNum);
+
+		arr = new int[arr_size];
+		randDataInitialization(arr, arr_size);
+	}  
+}
+
+
+void MergeCalculation(int*& pArr, int& pArr_size) {
+    
+    int curr_ProcNum = ProcNum;
+    int step = 1;
+
+    while (curr_ProcNum > 1) {
+        curr_ProcNum = curr_ProcNum / 2 + curr_ProcNum % 2;
+
+        if ((ProcRank - step) % (2 * step) == 0) {
+            MPI_Send(&pArr_size, 1, MPI_INT, ProcRank - step, 0,  MPI_COMM_WORLD);
+            MPI_Send(pArr, pArr_size, MPI_INT, ProcRank - step, 0, MPI_COMM_WORLD);
+        }
+
+        if ((ProcRank % (2 * step) == 0) && (ProcNum - ProcRank > step)) {
+            MPI_Status status;
+            int newSize;
+
+            MPI_Recv(&newSize, 1, MPI_INT, ProcRank + step, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            int* temp = new int[pArr_size + newSize];
+            MPI_Recv(temp, newSize, MPI_INT, ProcRank + step, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            
+            for (int i = 0; i < pArr_size; i++) {
+                temp[i + newSize] = pArr[i];
+            }
+
+            merge(temp, 0, newSize - 1, newSize + pArr_size - 1);
+
+            pArr = new int[newSize + pArr_size];
+
+            for (int i = 0; i < newSize + pArr_size; i++) {
+                pArr[i] = temp[i];
+            }           
+
+            pArr_size += newSize;
+            
+            delete[] temp;
+        }
+
+        step *= 2;
+    }
+    
+}
+
+void ResultReceive(int* pArr, int arr_size, int* res) {
+    int* recv_cnt = new int[ProcNum];
+    int* recv_disp = new int[ProcNum];
+    int cnt = arr_size / ProcNum;
+    int RestElem = arr_size;
+
+    recv_cnt[0] = cnt;
+    recv_disp[0] = 0;
+
+    for (int i = 1; i < ProcNum; i++) {
+        RestElem -= cnt;
+        cnt = RestElem / (ProcNum - i);
+        recv_cnt[i] = cnt;
+        recv_disp[i] = recv_cnt[i - 1] + recv_disp[i - 1];
+    }
+
+    MPI_Allgatherv(pArr, recv_cnt[ProcRank], MPI_INT,
+        res, recv_cnt, recv_disp, MPI_INT, MPI_COMM_WORLD);
+
+    delete[] recv_cnt;
+    delete[] recv_disp;
+}
+
+
+void DistrCalculation(int* pArr, int pArr_size) {
+    mergeSort(pArr, 0, pArr_size - 1);
+}
+
 // Function for computational process termination
 void ProcessTermination(double* pMatrix, double* pVector, double* pResult,
     double* pProcRows, double* pProcResult) {
@@ -88,9 +208,9 @@ int main(int argc, char* argv[])
 	ProccesInitialization(arr, arr_size);
 
     pStart = MPI_Wtime();
-    /*
-    somehow distribute sublists
-    */
+    DataDistr(arr, arr_size, pArr, pArr_size);
+    DistrCalculation(pArr, pArr_size);
+    MergeCalculation(pArr, pArr_size);
     pFinish = MPI_Wtime();
 
     pDuration = pFinish - pStart;
